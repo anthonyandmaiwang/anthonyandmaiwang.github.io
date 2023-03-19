cbe0b4ba9cef62d019bd986d93e69cc3dbe5e1ea
Anthony Wang <anthonywang@Anthonys-MacBook-Pro.local>
2023-03-19 00:12:43 -0700

    asdf

---
+++ component---src-templates-page-js-38990c3fcf41e2937006.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwedding=self.webpackChunkwedding||[]).push([[565],{6491:function(e,n,t){t.r(n),t.d(n,{Head:function(){return a}});var u=t(7294),r=t(670),c=t(6404);function a(e){return u.createElement(c.Z,e)}n.default=e=>u.createElement(r.Z,e)}}]);
+//# sourceMappingURL=component---src-templates-page-js-38990c3fcf41e2937006.js.map
